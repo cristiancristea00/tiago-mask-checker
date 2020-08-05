95c31c1da65299222f4e949689e8d252670aa189
Cristian Cristea <cristiancristea00@gmail.com>
2020-08-05 16:03:01 +0300

    Added comments

--- tiago.py
+++ tiago.py
@@ -3,6 +3,7 @@ from utils import *
 import argparse
 import sys
 
+# Defines the command line arguments
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('-t', '--tracker', type = str, default = 'CSRT',
 						help = 'Tracker type: BOOSTING/MIL/KCF/TLD/MEDIANFLOW/MOSSE/CSRT')
@@ -18,38 +19,51 @@ arg_parser.add_argument('-s', '--state', type = int, default = 10,
 						help = 'Number of frames to wait before a message is displayed')
 args = vars(arg_parser.parse_args())
 
+# Initialize the atomic wrappers thar are used to acquire the current frame
 normal_wrapper = AtomicWrapper()
 temp_wrapper = AtomicWrapper()
 thermal_wrapper = AtomicWrapper()
 
+# Define the 3 variables that hold the data for the current frame
 global thermal
 global normal
 global temp
 
 
 def callback_normal(data):
+	"""Gets the normal image from the robot, decompress it and crops it to fit
+	the temperature data."""
 	normal_wrapper.set(cv.imdecode(np.fromstring(data.data, np.uint8), cv.IMREAD_COLOR))
 	global normal
 	normal = normal_wrapper.get()[120:355, 150:500]
 
 
 def callback_thermal(data):
+	"""Gets the thermal image from the robot, decompress it and rescale to fit
+	the temperature data."""
 	thermal_wrapper.set(cv.imdecode(np.fromstring(data.data, np.uint8), cv.IMREAD_COLOR))
 	global thermal
 	thermal = cv.resize(thermal_wrapper.get(), (350, 235), interpolation = cv.INTER_NEAREST)
 
 
 def callback_temp(data):
+	"""Gets the temperature data from the robot, decompress it and rescale to
+	fit the temperature data."""
 	temp_wrapper.set(cv.imdecode(np.fromstring(data.data, np.uint8), cv.IMREAD_ANYDEPTH))
 	global temp
 	temp = cv.resize(temp_wrapper.get(), (350, 235), interpolation = cv.INTER_NEAREST)
 
 
 def video():
+	"""Principal method of the program that reads the data streams, displays
+	the video streams to the user and other messages."""
 	global thermal
 	global normal
 	global temp
 
+	# Define the variable that remember the current state: 'waiting' that awaits
+	# for a person to enter the frame and 'person_detected' in which checks
+	# continuously if the wearer's mask is worn correctly.
 	current_state = 'waiting'
 
 	tracker = Tracker(args['tracker'])
@@ -60,7 +74,7 @@ def video():
 									args['state'])
 
 	while True:
-		# Get current images
+		# Get current frames
 		normal_wrapper.set(normal)
 		curr_normal = normal_wrapper.get()
 		temp_wrapper.set(temp)
@@ -68,33 +82,39 @@ def video():
 		thermal_wrapper.set(thermal)
 		curr_thermal = thermal_wrapper.get()
 
+		# While in the 'waiting' state check if a person is in the frame
 		if current_state == 'waiting':
 			person_waiter.run_prediction(curr_normal)
 
+		# If a person entered the frame, change the current state
 		if person_waiter.person_in_frame():
 			current_state = 'person_detected'
 
+		# While in the 'person_detected' state check if the person is wearing
+		# the mask properly.
 		if current_state == 'person_detected':
 			person_checker.check_person(curr_normal, curr_temp)
 			if person_checker.mask_ok:
 				print(f'{person_checker.temp_checker.get_temp()} C')
-				current_state = 'waiting'
 				reset(person_waiter, person_checker, tracker, temp_checker)
+				current_state = 'waiting'
 
 		frame = np.vstack((curr_normal, curr_thermal))
 
-		# Display the result
+		# Display the concatenated current frame
 		cv.imshow('Video stream', frame)
 
 		# Exit if Q pressed
 		if cv.waitKey(1) & 0xFF == ord('q'):
 			break
 
+	# Close the video stream and exit the program
 	cv.destroyAllWindows()
 	sys.exit(0)
 
 
 def robot():
+	"""Main method that initializes the node and connect to data streams"""
 	rospy.init_node('robot', anonymous = True)
 	rospy.Subscriber('/xtion/rgb/image_raw/compressed', CompressedImage, callback_normal)
 	rospy.Subscriber('/optris/thermal_image/compressed', CompressedImage, callback_temp)
--- utils.py
+++ utils.py
@@ -4,67 +4,74 @@ from tensorflow.keras.models import load_model
 from control_msgs.msg import PointHeadActionGoal
 from geometry_msgs.msg import Point
 from threading import Lock, Thread
-from math import sqrt
 import numpy as np
 import cv2 as cv
 import rospy
 
 
 class AtomicWrapper:
+	"""Wrapper that uses the lock mechanism to acquire the current frame."""
+
 	def __init__(self):
+		"""Initialize the object and lock."""
 		self.obj = None
 		self.lock = Lock()
 
 	def set(self, obj):
+		"""Gets a copy of the object with the lock acquired."""
 		with self.lock:
 			self.obj = obj.copy()
 
 	def get(self):
+		"""Returns a copy of the object."""
 		return self.obj.copy()
 
 
 class FaceAndMaskDetector:
+	"""Class that encapsulates the functionality of the face and mask detector
+	and it's used to get the predictions of the network."""
+
 	def __init__(self, confidence):
+		"""Loads the face and mask models and sets the confidence level."""
 		self.face_net = cv.dnn.readNet('FaceNet.prototxt', 'FaceNet.caffemodel')
 		self.mask_net = load_model('mask_detector.model')
 		self.confidence = confidence
 
 	def detect_and_predict(self, frame):
-		# grab the dimensions of the frame and then construct a blob
-		# from it
+		"""Gets the current frame and returns the predictions and their
+		corresponding locations."""
+		# Grab the dimensions of the frame and then construct a blob from it
 		(h, w) = frame.shape[:2]
 		blob = cv.dnn.blobFromImage(frame, 1.0, (235, 350))
 
-		# pass the blob through the network and obtain the face detections
+		# Pass the blob through the network and obtain the face detections
 		self.face_net.setInput(blob)
 		detections = self.face_net.forward()
 
-		# initialize our list of faces, their corresponding locations,
-		# and the list of predictions from our face mask network
+		# Initialize our list of faces, their corresponding locations, and the
+		# list of predictions from our face mask network
 		faces = []
 		locations = []
 		predictions = []
 
-		# loop over the detections
+		# Loop over the detections
 		for i in range(0, detections.shape[2]):
-			# extract the confidence (i.e., probability) associated with
-			# the detection
+			# Extract the confidence associated with the detection
 			confidence = detections[0, 0, i, 2]
 
-			# filter out weak detections by ensuring the confidence is
-			# greater than the minimum confidence
+			# Filter out weak detections by ensuring the confidence is greater
+			# than the minimum confidence
 			if confidence > self.confidence:
-				# compute the (x, y)-coordinates of the bounding box for
-				# the object
+				# Compute the coordinates of the bounding box for the object
 				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 				(start_x, start_y, end_x, end_y) = box.astype('int')
 
-				# ensure the bounding boxes fall within the dimensions of
+				# Ensure the bounding boxes fall within the dimensions of
 				# the frame
 				(start_x, start_y) = (max(0, start_x), max(0, start_y))
 				(end_x, end_y) = (min(w - 1, end_x), min(h - 1, end_y))
 
-				# extract the face ROI, convert it from BGR to RGB channel
+				# Extract the face ROI, convert it from BGR to RGB channel
 				# ordering, resize it to 224x224, and pre-process it
 				face = frame[start_y:end_y, start_x:end_x]
 				face = cv.cvtColor(face, cv.COLOR_BGR2RGB)
@@ -73,31 +80,33 @@ class FaceAndMaskDetector:
 				face = preprocess_input(face)
 				face = np.expand_dims(face, axis = 0)
 
-				# add the face and bounding boxes to their respective
-				# lists
+				# Add the face and bounding boxes to their respective lists
 				faces.append(face)
 				locations.append((start_x, start_y, end_x, end_y))
 
-		# only make a predictions if at least one face was detected
+		# Only make a predictions if at least one face was detected
 		if len(faces) > 0:
-			# for faster inference we'll make batch predictions on *all*
-			# faces at the same time rather than one-by-one predictions
-			# in the above `for` loop
+			# For faster inference we'll make batch predictions on all faces at
+			# the same time rather than one-by-one predictions
 			predictions = self.mask_net.predict(faces)
 
-		# return a 2-tuple of the face locations and their corresponding
-		# locations
+		# Return the face locations and their corresponding locations
 		return locations, predictions
 
 
 class Tracker:
+	"""Class that acts as a wrapper for the tracker object that makes it easier
+	to manipulate it."""
+
 	def __init__(self, name):
+		"""Sets the type of the tracker object and creates it."""
 		self.name = name
 		self.track_ok = False
 		self.internal_tracker = None
 		self.create_tracker()
 
 	def create_tracker(self):
+		"""Sets the tracker type based of its name."""
 		if self.name == 'CSRT':
 			self.internal_tracker = cv.TrackerCSRT_create()
 		elif self.name == 'BOOSTING':
@@ -114,121 +123,142 @@ class Tracker:
 			self.internal_tracker = cv.TrackerMOSSE_create()
 
 	def init(self, image, bbox):
+		"""Initializes the tracker on provided bounding box."""
 		return self.internal_tracker.init(image, bbox)
 
 	def update(self, image):
+		"""Updates the tracker on the current bounding box."""
 		return self.internal_tracker.update(image)
 
 	def reset(self):
+		"""Resets the tracker to its default state."""
 		self.create_tracker()
 
 
 class TemperatureChecker:
+	"""Class that stores the temperature data for the current person."""
+
 	def __init__(self):
-		self.temp_data = None
-		self.temps = []
+		self.temp_data = []
 
 	def add_data(self, image, start_x, start_y, end_x, end_y):
-		self.temp_data = image
+		"""Adds the temperature data from the forehead for the current frame"""
 		try:
 			t = np.max(image[start_y + 15:end_y - 35, start_x + 14:end_x - 14] - 1000) / 10.0
 		except ValueError:
 			t = 0
 		if t > 34:
-			self.temps.append(t)
+			self.temp_data.append(t)
 
 	def get_temp(self):
-		return np.round(np.mean(self.temps), 1)
+		"""Gets the temperature as a mean of the acquired data while the person
+		was in the frame."""
+		return np.round(np.mean(self.temp_data), 1)
 
 	def reset(self):
-		self.temps.clear()
+		"""Resets the temperature checker to its default state."""
+		self.temp_data.clear()
 
 
 class WaitingForPerson:
+	"""Class that describes the state in which the robot is waiting for a
+	person to enter the frame."""
+
 	def __init__(self, tracker, face_mask, wait_counter_init):
+		"""Initializes the tracker, detector and the variable that
+		stores if a person is in the frame"""
 		self.default_wait_counter_init = wait_counter_init
-		self.person_detected = False
-		self.bbox = None
 		self.wait_counter = wait_counter_init
 		self.detector = face_mask
 		self.tracker = tracker
+		self.person_detected = False
+		self.bbox = None
 
 	def run_prediction(self, image):
+		"""Runs the prediction on the current frame and starts the tracker if
+		a person is detected."""
+		# Run the prediction on the current frame
 		locations, predictions = self.detector.detect_and_predict(image)
 
 		# Decrement the wait counter for every frame where a face is detected
 		if len(predictions) != 0 and self.wait_counter != 0:
 			self.wait_counter -= 1
 
-		# Start tracker on the first face detected
+		# Start tracker on the first face detected and signal that a face is
+		# detected by modifying the variable 'person_detected'
 		if not self.person_detected and len(predictions) != 0 and self.wait_counter == 0:
 			self.person_detected = True
 			self.bbox = points_to_1point_and_dims(locations[0])
 			self.tracker.track_ok = self.tracker.init(image, self.bbox)
 
 	def person_in_frame(self):
+		"""Checks if a person is in frame."""
 		return self.person_detected
 
 	def reset(self):
+		"""Resets the state to its default parameters."""
 		self.wait_counter = self.default_wait_counter_init
-
 		self.person_detected = False
 		self.bbox = None
 
 
 class CheckingPerson(WaitingForPerson):
+	"""Class that describes the state in which the robot is checking if the
+	person's mask is worn correctly and prompts instructions for the person if
+	the mask is worn incorrectly, the checks the temperature."""
+
 	def __init__(self, tracker, face_mask, temp_checker, counter_init, wait_counter_init, dist_threshold, state_time):
+		"""Besides what 'WaitingForPerson' initializes, it also initializes the
+		temperature checker, states dictionary that holds the prediction type
+		for some set number of frames, and the variables that hold the
+		information if the instruction was said and if the mask is worn
+		correctly"""
 		WaitingForPerson.__init__(self, tracker, face_mask, wait_counter_init)
-		self.mask_ok = False
-		self.action_said = False
+		self.default_predictions = {'with_mask': 0, 'with_mask_no_nose': 0, 'with_mask_under': 0, 'no_mask': 0}
+		self.predictions = self.default_predictions.copy()
 		self.default_counter_init = counter_init
-		self.default_states = {'with_mask': 0, 'with_mask_no_nose': 0, 'with_mask_under': 0, 'no_mask': 0}
-		self.states = self.default_states.copy()
-		self.state_time = state_time
-		self.distance_threshold = dist_threshold
 		self.counter = counter_init
 		self.temp_checker = temp_checker
-
-	def add_state(self, state):
-		self.states[state] += 1
-
-	def get_max_state(self):
-		return max(self.states, key = self.states.get)
-
-	def reset_states(self):
-		self.states = self.default_states.copy()
+		self.distance_threshold = dist_threshold
+		self.state_time = state_time
+		self.action_said = False
+		self.mask_ok = False
 
 	@staticmethod
 	def prediction_type(prediction):
+		"""Gets the prediction type by checking which is the max probability."""
 		with_mask, with_mask_no_nose, with_mask_under, no_mask = prediction
-		max_prob = max(with_mask, with_mask_no_nose, with_mask_under, no_mask)
-		if max_prob == with_mask:
-			return 'with_mask', max_prob
-		elif max_prob == with_mask_no_nose:
-			return 'with_mask_no_nose', max_prob
-		elif max_prob == with_mask_under:
-			return 'with_mask_under', max_prob
+		probability = max(with_mask, with_mask_no_nose, with_mask_under, no_mask)
+		if probability == with_mask:
+			return 'with_mask', probability
+		elif probability == with_mask_no_nose:
+			return 'with_mask_no_nose', probability
+		elif probability == with_mask_under:
+			return 'with_mask_under', probability
 		else:
-			return 'no_mask', max_prob
+			return 'no_mask', probability
 
 	@staticmethod
-	def print_message(state):
-		if state == 'with_mask':
+	def print_message(prediction_type):
+		"""Print the corresponding message based on the prediction type."""
+		if prediction_type == 'with_mask':
 			print('Your mask is OK. Let\'s check your temperature now.')
-		elif state == 'with_mask_no_nose':
+		elif prediction_type == 'with_mask_no_nose':
 			print('Please cover your nose.')
-		elif state == 'with_mask_under':
+		elif prediction_type == 'with_mask_under':
 			print('Please don\'t user your mask as a chin guard.')
-		elif state == 'no_mask':
+		elif prediction_type == 'no_mask':
 			print('You can\'t enter without a mask.')
 
 	@staticmethod
 	def draw_detector(locations, predictions, image):
+		"""Draws the bounding boxes on the detected faces, along with the
+		probabilities of the prediction."""
 		label, color = None, None
 		for bbox, prediction in zip(locations, predictions):
 			# Unpack the bounding box and predictions
 			start_x, start_y, end_x, end_y = bbox
-			prediction_type, max_prob = CheckingPerson.prediction_type(prediction)
+			prediction_type, probability = CheckingPerson.prediction_type(prediction)
 			if prediction_type == 'with_mask':
 				label = 'Mask is OK'
 				color = (0, 255, 0)  # Green
@@ -243,7 +273,7 @@ class CheckingPerson(WaitingForPerson):
 				color = (0, 0, 255)  # Red
 
 			# Include the probability in the label
-			label = f'{label}: {max_prob * 100:.2f}%'
+			label = f'{label}: {probability * 100:.2f}%'
 
 			# Display the label and bounding box rectangle on the output frame
 			cv.putText(image, label, (start_x, start_y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
@@ -252,6 +282,7 @@ class CheckingPerson(WaitingForPerson):
 
 	@staticmethod
 	def draw_tracker(track_ok, image, bbox, tracker_type):
+		"""Draws th tracker bounding box on the face"""
 		if track_ok:
 			# Tracking success
 			points = point_and_dims_to_2points(bbox)
@@ -263,9 +294,23 @@ class CheckingPerson(WaitingForPerson):
 		# Display tracker type on frame
 		cv.putText(image, tracker_type + ' Tracker', (5, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (50, 170, 50), 2)
 
+	def add_prediction(self, prediction_type):
+		"""Increment by 1 in the dictionary the prediction type."""
+		self.predictions[prediction_type] += 1
+
+	def get_max_prediction(self):
+		"""Gets the maximum prediction."""
+		return max(self.predictions, key = self.predictions.get)
+
+	def reset_predictions(self):
+		"""Resets the predictions dictionary to its default state."""
+		self.predictions = self.default_predictions.copy()
+
 	def check_person(self, image, temp):
+		"""Checks the tracked person's mask in the current frame."""
 		locations, predictions = self.detector.detect_and_predict(image)
 
+		# Draw the detector bounding boxes
 		self.draw_detector(locations, predictions, image)
 
 		# Decrement the wait counter for every frame where a face is detected
@@ -282,41 +327,50 @@ class CheckingPerson(WaitingForPerson):
 		# For every set frames, reposition the tracker on the detected face if
 		# the distance between their centers are under the threshold value
 		if self.counter == 0 and self.tracker.track_ok:
+			# Reset the counter to the default value
 			self.counter = self.default_counter_init
+			# Get the tracker bounding box center
 			tracker_center = get_center(point_and_dims_to_2points(self.bbox))
 			for box, prediction in zip(locations, predictions):
 				start_x, start_y, end_x, end_y = box
 				detector_center = get_center(((start_x, start_y), (end_x, end_y)))
+				# Check if the threshold value is met
 				if dist(tracker_center, detector_center) <= self.distance_threshold:
 					self.bbox = points_to_1point_and_dims((start_x, start_y, end_x, end_y))
+					# Reinitialize the tracker
 					self.tracker.create_tracker()
 					self.tracker.track_ok = self.tracker.init(image, self.bbox)
-
+					# Get the temperature for the current frame
 					self.temp_checker.add_data(temp, start_x, start_y, end_x, end_y)
+					# Add the prediction type
 					prediction_type, _ = self.prediction_type(prediction)
-					self.add_state(prediction_type)
-					max_state = self.get_max_state()
-					if not self.action_said and self.states[max_state] >= self.state_time:
+					self.add_prediction(prediction_type)
+					max_state = self.get_max_prediction()
+					# Print the message
+					if not self.action_said and self.predictions[max_state] >= self.state_time:
 						self.print_message(max_state)
 						self.action_said = True
 						if max_state == 'with_mask':
 							self.mask_ok = True
-					elif self.action_said and self.states[max_state] >= self.state_time:
+					# If the message was already printed, check again if the
+					# mask is worn correctly
+					elif self.action_said and self.predictions[max_state] >= self.state_time:
 						if max_state == 'with_mask':
 							print('You are okay now. Let\'s check your temperature.')
 							self.mask_ok = True
 						else:
-							self.reset_states()
+							self.reset_predictions()
 					break
-
+		# Draw the tracker bounding box
 		self.draw_tracker(self.tracker.track_ok, image, self.bbox, self.tracker.name)
 
 	def reset(self):
+		"""Resets the state to its default parameters."""
 		WaitingForPerson.reset(self)
 		self.counter = self.default_counter_init
 		self.mask_ok = False
 		self.action_said = False
-		self.states = self.default_states.copy()
+		self.reset_predictions()
 
 	def person_in_frame(self):
 		raise AttributeError('\'CheckingPerson\' has no attribute named \'person_in_frame\'')
@@ -325,8 +379,9 @@ class CheckingPerson(WaitingForPerson):
 		raise AttributeError('\'CheckingPerson\' has no attribute named \'run_prediction\'')
 
 
-# converts opposite points of a rectangle to 1 point and width and height
 def points_to_1point_and_dims(bbox):
+	"""Converts opposite points of a rectangle to 1 point and its dimensions,
+	width and height."""
 	box_x = bbox[0]
 	box_y = bbox[1]
 	box_width = bbox[2] - bbox[0]
@@ -334,27 +389,29 @@ def points_to_1point_and_dims(bbox):
 	return box_x, box_y, box_width, box_height
 
 
-# the inverse of the above function
 def point_and_dims_to_2points(bbox):
+	"""Converts 1 point and its dimensions, width and height, of a rectangle to
+	opposite points."""
 	p1 = (int(bbox[0]), int(bbox[1]))
 	p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
 	return p1, p2
 
 
-# returns the middle point of a rectangle
 def get_center(bbox):
+	"""Returns the center point of a rectangle."""
 	(start_x, start_y), (end_x, end_y) = bbox
 	center_x = int((start_x + end_x) / 2)
 	center_y = int((start_y + end_y) / 2)
 	return center_x, center_y
 
 
-# return the euclidean distance between 2 points
 def dist(p1, p2):
-	return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+	"""Returns the euclidean distance between 2 points."""
+	return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
 
 def reset(person_waiter, person_checker, tracker, temp_checker):
+	"""Resets the instances to their initial state."""
 	person_waiter.reset()
 	person_checker.reset()
 	tracker.reset()
