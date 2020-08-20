43f58ecdec7dc62e2c3f2768b19a5e638dfe27c3
Cristian Cristea <cristiancristea00@gmail.com>
2020-08-20 12:06:33 +0300

    Updated the Looker class

--- tiago.py
+++ tiago.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from sensor_msgs.msg import CompressedImage
 from utils.atomic_wrapper import AtomicWrapper
 from utils.face_and_mask_detector import FaceAndMaskDetector
@@ -6,14 +8,14 @@ from utils.temperature_checker import TemperatureChecker
 from utils.looker import Looker
 from utils.waiting_for_person import WaitingForPerson
 from utils.checking_person import CheckingPerson
-import numpy as np
+from numpy import fromstring, vstack, uint8
+from argparse import ArgumentParser
 import cv2 as cv
-import argparse
 import rospy
 import sys
 
 # Defines the command line arguments
-arg_parser = argparse.ArgumentParser()
+arg_parser = ArgumentParser()
 arg_parser.add_argument('-t', '--tracker', type = str, default = 'CSRT',
 						help = 'Tracker type: BOOSTING/MIL/KCF/TLD/MEDIANFLOW/MOSSE/CSRT.')
 arg_parser.add_argument('-c', '--confidence', type = float, default = 0.5,
@@ -42,7 +44,7 @@ global temp
 def callback_normal(data):
 	"""Gets the normal image from the robot, decompress it and crops it to fit
 	the temperature data."""
-	normal_wrapper.set(cv.imdecode(np.fromstring(data.data, np.uint8), cv.IMREAD_COLOR))
+	normal_wrapper.set(cv.imdecode(fromstring(data.data, uint8), cv.IMREAD_COLOR))
 	global normal
 	normal = normal_wrapper.get()[120:355, 150:500]
 
@@ -50,7 +52,7 @@ def callback_normal(data):
 def callback_thermal(data):
 	"""Gets the thermal image from the robot, decompress it and rescale to fit
 	the temperature data."""
-	thermal_wrapper.set(cv.imdecode(np.fromstring(data.data, np.uint8), cv.IMREAD_COLOR))
+	thermal_wrapper.set(cv.imdecode(fromstring(data.data, uint8), cv.IMREAD_COLOR))
 	global thermal
 	thermal = cv.resize(thermal_wrapper.get(), (350, 235), interpolation = cv.INTER_NEAREST)
 
@@ -58,7 +60,7 @@ def callback_thermal(data):
 def callback_temp(data):
 	"""Gets the temperature data from the robot, decompress it and rescale to
 	fit the temperature data."""
-	temp_wrapper.set(cv.imdecode(np.fromstring(data.data, np.uint8), cv.IMREAD_ANYDEPTH))
+	temp_wrapper.set(cv.imdecode(fromstring(data.data, uint8), cv.IMREAD_ANYDEPTH))
 	global temp
 	temp = cv.resize(temp_wrapper.get(), (350, 235), interpolation = cv.INTER_NEAREST)
 
@@ -117,7 +119,7 @@ def video():
 				reset(person_waiter, person_checker, tracker, temp_checker)
 				current_state = 'waiting'
 
-		frame = np.vstack((curr_normal, curr_thermal))
+		frame = vstack((curr_normal, curr_thermal))
 
 		# Display the concatenated current frame
 		cv.imshow('Video stream', frame)
--- utils/looker.py
+++ utils/looker.py
@@ -1,5 +1,7 @@
 from control_msgs.msg import PointHeadActionGoal
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import PointStamped
+from sensor_msgs.msg import CameraInfo
+from numpy import array
 from threading import Thread
 import rospy
 
@@ -11,18 +13,28 @@ class Looker(Thread):
 		"""Initializes the data stream that sends the instructions to the root
 		and the point that the robot will look at."""
 		Thread.__init__(self)
+		self.rate = rospy.Rate(5)
 		self.pub = rospy.Publisher('/head_controller/point_head_action/goal', PointHeadActionGoal, queue_size = 1)
 		self.looker = PointHeadActionGoal()
-		self.looker.header.frame_id = '/base_link'
-		self.looker.goal.target.header.frame_id = '/base_link'
-		self.looker.goal.pointing_frame = '/head_2_link'
-		self.looker.goal.max_velocity = 0.2
-		self.look_point = Point()
-		self.look_point.x = 15
-		self.look_point.y = 0
-		self.look_point.z = 0
-		self.looker.goal.target.point = self.look_point
-		self.rate = rospy.Rate(5)
+		self.looker.goal.pointing_axis.x = 0.0
+		self.looker.goal.pointing_axis.y = 0.0
+		self.looker.goal.pointing_axis.z = 0.0
+		self.looker.goal.max_velocity = 0.3
+
+		self.point = PointStamped()
+		self.point.header.frame_id = '/xtion_rgb_optical_frame'
+		self.point.header.stamp = None
+
+		self.point.point.x = 0.0
+		self.point.point.y = 0.0
+		self.point.point.z = 15.0
+
+		cam_info_topic = rospy.get_param('/xtion/rgb/camera_info', CameraInfo)
+		camera_info_msg = rospy.wait_for_message(cam_info_topic, CameraInfo)
+		self.camera_intrinsics = array(camera_info_msg.K).reshape((3, 3))
+
+		self.looker.goal.target.point = self.point
+
 		self.running = True
 		self.daemon = True
 		self.start()
@@ -36,3 +48,21 @@ class Looker(Thread):
 	def stop(self):
 		"""Stops the infinite loop."""
 		self.running = False
+
+	def point_head(self, point_1, point_2):
+		x = (point_1 - self.camera_intrinsics[0, 2]) / (self.camera_intrinsics[0, 0])
+		y = (point_2 - self.camera_intrinsics[1, 2]) / (self.camera_intrinsics[1, 1])
+		z = 1.0
+
+		self.point.point.x = x * z
+		self.point.point.y = y * z
+		self.point.point.z = z
+
+		self.looker.goal.target.point = self.point
+
+	def reset(self):
+		self.point.point.x = 0.0
+		self.point.point.y = 0.0
+		self.point.point.z = 15.0
+
+		self.looker.goal.target.point = self.point
